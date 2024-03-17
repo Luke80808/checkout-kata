4536a321535f502689393cc37ca738e51755e596
LukeJ <luke.jamieson8@gmail.com>
2024-03-17 13:02:34 +0000

    Add project files.

---
+++ checkout-kata/Classes/PricingRule.cs
@@ -0,0 +1,9 @@
+﻿
+namespace checkout_kata.Classes;
+
+public class PricingRule(char item, int count, decimal price)
+{
+    public readonly char Item = item;
+    public readonly int Count = count;
+    public readonly decimal Price = price;
+}
