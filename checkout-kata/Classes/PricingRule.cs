adcd717309a54390519cdbbe69a5af6e6377e1c9
LukeJ <luke.jamieson8@gmail.com>
2024-03-17 14:50:00 +0000

    Update pricing rule items to strings

--- checkout-kata/Classes/PricingRule.cs
+++ checkout-kata/Classes/PricingRule.cs
@@ -1,9 +1,9 @@
 ﻿
 namespace checkout_kata.Classes;
 
-public class PricingRule(char item, int count, decimal price)
+public class PricingRule(string item, int count, decimal price)
 {
-    public readonly char Item = item;
+    public readonly string Item = item;
     public readonly int Count = count;
     public readonly decimal Price = price;
 }
