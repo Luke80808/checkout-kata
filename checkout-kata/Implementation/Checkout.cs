0a557fa90372ed15b0b5c4fc8683afb85d3a115a
LukeJ <luke.jamieson8@gmail.com>
2024-03-17 14:38:03 +0000

    Add Checkout constructor

--- checkout-kata/Implementation/Checkout.cs
+++ checkout-kata/Implementation/Checkout.cs
@@ -1,9 +1,18 @@
-﻿using checkout_kata.Interfaces;
+﻿using checkout_kata.Classes;
+using checkout_kata.Interfaces;
 
 namespace checkout_kata.Implementation;
 
 public class Checkout : ICheckout
 {
+    private PricingRule[] _pricingRules;
+    private List<string> _items;
+
+    public Checkout(IEnumerable<PricingRule> pricingRules)
+    {
+        _pricingRules = pricingRules.ToArray();
+        _items = [];
+    }
     public void Scan(string item)
     {
         throw new NotImplementedException();
