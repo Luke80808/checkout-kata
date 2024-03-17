7a5f642f79b3127574c364c6126d6cc0c655fb14
LukeJ <luke.jamieson8@gmail.com>
2024-03-17 14:50:27 +0000

    Update GetTotalPrice method to return a decimal

--- checkout-kata/Implementation/Checkout.cs
+++ checkout-kata/Implementation/Checkout.cs
@@ -18,8 +18,25 @@ public class Checkout : ICheckout
         throw new NotImplementedException();
     }
 
-    public int GetTotalPrice()
+    public decimal GetTotalPrice()
     {
-        throw new NotImplementedException();
+        decimal total = 0m;
+        var itemsGroups = _items.GroupBy(x => x);
+
+        foreach (var @group in itemsGroups)
+        {
+            var rules = _pricingRules
+                .Where(r => r.Item == @group.Key)
+                .OrderByDescending(r => r.Count);
+            var itemCount = @group.Count();
+
+            while (itemCount > 0)
+            {
+                var ruleToApply = rules.First(r => r.Count <= itemCount);
+                total += ruleToApply.Price;
+                itemCount -= ruleToApply.Count;
+            }
+        }
+        return total;
     }
 }
